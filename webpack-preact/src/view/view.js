927a9ead715b7625708b65a4334d8e646873cdc8
cjp2600 <icjp2600@gmail.com>
2023-01-20 19:07:20 +0300

    webpack-react plugin sample

--- webpack-preact/src/view/view.js
+++ webpack-preact/src/view/view.js
@@ -1,9 +1,32 @@
-import { h, render } from "preact";
+import { h, Component, render } from "preact";
 
 import "./view.css";
 
-const View = () => {
-    return <h1 className={"title"}>View -- {$_bx.get("title")}</h1>;
-};
+class View extends Component {
+
+    constructor() {
+        super();
+        this.state = { time: Date.now() };
+    }
+
+    // Lifecycle: Called whenever our component is created
+    componentDidMount() {
+        // update time every second
+        this.timer = setInterval(() => {
+            this.setState({ time: Date.now() });
+        }, 1000);
+    }
+
+    // Lifecycle: Called just before our component will be destroyed
+    componentWillUnmount() {
+        // stop when not renderable
+        clearInterval(this.timer);
+    }
+
+    render() {
+        let time = new Date(this.state.time).toLocaleTimeString();
+        return <span className={"title"}>{time}</span>;
+    }
+}
 
 render(<View />, document.querySelector("#root"));
---
+++ webpack-react/src/view/view.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./view.css";
+
+declare function require(path: string): any;
+
+function App() {
+    const inputRef = React.useRef<HTMLInputElement>(null);
+
+    const onCreate = () => {
+        const count = Number(inputRef.current?.value || 0);;
+    };
+
+    const onCancel = () => {
+
+    };
+
+    return (
+        <main>
+            <header>
+                <img src={require("../logo.svg")} />
+                <h2>Rectangle Creator</h2>
+            </header>
+            <section>
+                <input id="input" type="number" min="0" ref={inputRef} />
+                <label htmlFor="input">Rectangle Count</label>
+            </section>
+            <footer>
+                <button className="brand" onClick={onCreate}>
+                    Create
+                </button>
+                <button onClick={onCancel}>Cancel</button>
+            </footer>
+        </main>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("react-page"));
